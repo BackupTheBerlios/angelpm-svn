94979917e4f4a3599752ae9ba057e4f18adf1f4e
anticlockwise_5 <anticlockwise_5@93a5066c-7b25-0410-bc64-b6dc1f9dbebe>
2010-04-17 21:58:32 +0000

    Restructuring

--- angel/xmpp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from collabnote.xmpp.models import *
-
-class OfUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'name', 'email')
-
-admin.site.register(OfUser, OfUserAdmin)
